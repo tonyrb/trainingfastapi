dc5870b804699bfe7bb7373768d589e72fe96704
Tony <tony.rab@gmail.com>
2021-11-01 19:43:18 +0100

    add database postgresql

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,21 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
     PROJECT_TITLE:  str = "Jobboard"
     PROJECT_VERSION :   str = "0.1.0"
+
+    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD : str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER=str = os.getenv("POSTGRES_SERVER", "localhost")
+    PSOTGRES_PORT = str = os.getenv("PSOTGRES_PORT", 5432)
+    POSTGRES_DB = str = os.getenv("POSTGRES_DB", "db_course")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{PSOTGRES_PORT}/{POSTGRES_DB}"
 
 
 settings = Settings(
